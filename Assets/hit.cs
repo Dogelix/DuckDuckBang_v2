6477493b9727b026865e5a7604d2d25a76cf2722
Rad5002 <ryan5002@hotmail.co.uk>
2020-02-12 16:27:31 +0000

    Quick Start on Menu
    - Dabbled in Menu, VR, Video Player, and collisions

---
+++ Assets/hit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class hit : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Bullet")
+        {
+            // Destroy bullet too
+            Destroy(collision.gameObject);
+            //Loads scene
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
---
+++ Assets/new_scene_1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class new_scene_1 : MonoBehaviour
+{
+    public void NewScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
