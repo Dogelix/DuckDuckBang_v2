29c904f862234d051a2a78bda83fe39f1ec35456
Dogelix <yeatmanjake@gmail.com>
2020-02-13 15:33:58 +0000

    Event Messaging System

--- Assets/Scripts/Other/Testing.cs
+++ Assets/Scripts/Other/Testing.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Testing : MonoBehaviour
 {
+    //private UnityAction action;
+
+    //private void Awake()
+    //{
+    //    action = new UnityAction(SomeOtherFunction);   
+    //}
+
+    //private void OnEnable()
+    //{
+    //    EventManager.StartListening("test", action);
+    //}
+
+
     // Start is called before the first frame update
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject.FindGameObjectsWithTag("Bread").First().GetComponentInChildren<TargetHealth>().TakeDamage();
-        }
+        
     }
+
+    //void SomeOtherFunction()
+    //{
+    //    Debug.Log("Some Other Function was called!");
+    //}
 }
