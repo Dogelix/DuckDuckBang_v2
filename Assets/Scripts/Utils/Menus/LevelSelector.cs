3b5414b627194f274868e0c492a9fffe0fe49104
Dogelix <yeatmanjake@gmail.com>
2020-03-04 20:24:54 +0000

    Menu

---
+++ Assets/Scripts/Utils/Menus/LevelSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelector : MenuParentClass
+{
+    public override void Activate()
+    {
+        SceneManager.LoadSceneAsync(_parameters[0]);
+    }
+}
