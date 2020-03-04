67e7e66154acfd1b43611d3b1de664fab374aba0
Rad5002 <ryan5002@hotmail.co.uk>
2020-03-04 18:28:14 +0000

    Golden Regi Start
    > Basic Prefabs
    > Basic Scripts
    > Spawn & End Locations
    > Spawns Golden Regi Prefab Clone
    > Golden Regi Moves
    > Golden Regi does not collide with endPortal for some reason

---
+++ Assets/Scripts/GolRegi/GolRegiSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolRegiSpawner : MonoBehaviour
+{
+
+    // Tranforms are the positions of spawn and end locations
+    public Transform[] spawnLocations;
+    public Transform[] endLocations;
+    // Prefabs as array. 0 = GoldenRegi, 1 = spawnLocation, 2 = EndLocation
+    public GameObject[] prefabs;
+    // Clones as array. Same as Prefabs.
+    public GameObject[] clones;
+    int spawn = 0;
+
+    void Start()
+    {
+        RandomGenerate();
+        spawnPortals();
+        spawnGoldenRegi();
+    }
+
+    /// <summary>
+    /// Spawns clones of the spawn & end portals in at the positions. Portals should be grouped in unity e.g. duck spawns at sp00 and goes to ep00
+    /// </summary>
+    void spawnPortals()
+    {
+        clones[1] = Instantiate(prefabs[1], spawnLocations[spawn].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        clones[2] = Instantiate(prefabs[2], endLocations[spawn].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+
+    /// <summary>
+    /// Spawns a clone of the Prefab Golden Regi at a spawn location chosen by spawn, rotated to face position (QE)
+    /// </summary>
+    void spawnGoldenRegi()
+    {
+        clones[0] = Instantiate(prefabs[0], spawnLocations[spawn].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+    }
+
+
+    /// <summary>
+    /// Sets the Randomly Generated Number to use for Spawn and End Locations.
+    /// </summary>
+    void RandomGenerate()
+    {
+        spawn = Random.Range(0, 1);
+        Debug.Log(spawn);
+    }
+}
---
+++ Assets/Scripts/GolRegi/golRegiBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class golRegiBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime;
+    }
+
+    int collideTrue = 0;
+
+    /// <summary>
+    /// If collides with EndPortal, Destroys GoldenRegi and EndPortal
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.tag == "endPortal")
+        {
+            collideTrue = 1;
+            Debug.Log(collideTrue);
+            // Destroy GoldenRegi & Portal
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
