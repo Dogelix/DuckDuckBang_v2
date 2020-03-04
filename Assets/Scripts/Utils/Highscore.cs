1fbd0cb64611e9201dad705f0992932fcc58fa4f
Dogelix <yeatmanjake@gmail.com>
2020-03-04 19:31:39 +0000

    Quack

--- Assets/Scripts/Utils/Highscore.cs
+++ Assets/Scripts/Utils/Highscore.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Bson;
 using System.IO;
 
 public class Highscore : MonoBehaviour
