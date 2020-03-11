0f9c1b0f518956f073d3efdc7e8bc4b769c35c10
powellsky <toneception@gmail.com>
2020-03-11 18:26:27 +0000

    Bullet Time added

--- Assets/PowerUpController.cs
+++ Assets/PowerUpController.cs
@@ -9,12 +9,12 @@ public class PowerUpController : MonoBehaviour
 {
     public SteamVR_Action_Boolean useAction = null;
     public List<Sprite> PowerUpImages;
+    public float BulletTimeDuration;
+    public float bulletTimeScale;
 
     private PowerUps currentPowerUp = PowerUps.None;
     private SteamVR_Behaviour_Pose _pose;
-    private bool isDisplayed;
     private Image imageHolder;
-
 
     // Start is called before the first frame update
     void Start()
@@ -26,41 +26,34 @@ public class PowerUpController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {       
-        DisplayPowerUp();
         UsePowerUp();
-    }
-
-    private void DisplayPowerUp()
-    {
-        if (!isDisplayed && currentPowerUp != PowerUps.None)
-        {
-            imageHolder.enabled = true;
-            imageHolder.sprite = PowerUpImages.FirstOrDefault(x => x.name == currentPowerUp.ToString());           
-            isDisplayed = true;
-        }
     }
 
     private void UsePowerUp()
     {
-        if (isDisplayed && currentPowerUp != PowerUps.None && useAction.GetStateDown(_pose.inputSource)) // Use
+        if (currentPowerUp != PowerUps.None && useAction.GetStateDown(_pose.inputSource) ) // Use
         {
-
-            GetType().GetMethod(currentPowerUp.ToString()).Invoke(this, null); // Calls method by its name (corresponds to Enum names). Avoids use of a switch statement
-
-            isDisplayed = false;
-            currentPowerUp = PowerUps.None;
-            imageHolder.enabled = false;
+            GetType().GetMethod(currentPowerUp.ToString()).Invoke(this, null); // Calls method by its name (corresponds to Enum names). Avoids use of a switch statement           
         }
     }
 
     public void EnablePowerUp(PowerUps powerUp)
     {
         currentPowerUp = powerUp;
+
+        if ( currentPowerUp != PowerUps.None)
+        { // Display image
+            imageHolder.enabled = true;
+            imageHolder.sprite = PowerUpImages.FirstOrDefault(x => x.name == currentPowerUp.ToString());
+        }
     }
 
     #region PowerUps Core Logic
     public void DestroyAll()
     {
+        imageHolder.enabled = false;
+        currentPowerUp = PowerUps.None;
+
         int flyCount = Flock.agents.Count();
 
         for (int i = flyCount - 1; i >= 0; i--)
@@ -77,12 +70,29 @@ public class PowerUpController : MonoBehaviour
             Destroy(b); // Points script handles OnDestroy and does rest of the logic.
         }
     }
+
+    public void BulletTime()
+    {
+        imageHolder.enabled = false;
+        currentPowerUp = PowerUps.None;
+
+        Time.timeScale = bulletTimeScale;
+
+        StartCoroutine(StopBulletTime());
+    }
+
+    private IEnumerator StopBulletTime()
+    {
+        yield return new WaitForSeconds(BulletTimeDuration * Time.timeScale);
+        Time.timeScale = 1.0f;
+    }
+
     #endregion
 
     public enum PowerUps
     {
         DestroyAll,
+        BulletTime,
         None
     }
-
 }
