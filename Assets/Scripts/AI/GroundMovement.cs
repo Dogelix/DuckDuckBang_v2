fe1921d3f6c8ed16ecd06b30af23d9734bf9b8e8
Dogelix <yeatmanjake@gmail.com>
2020-03-11 20:05:34 +0000

    Animations

--- Assets/Scripts/AI/GroundMovement.cs
+++ Assets/Scripts/AI/GroundMovement.cs
@@ -10,6 +10,8 @@ public class GroundMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponentInChildren<Animator>().SetTrigger("ZombieWalk");
+
         SetTarget();
     }
 
