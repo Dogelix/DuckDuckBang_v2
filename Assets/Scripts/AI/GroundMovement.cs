f7a438dce5f8094bfd9696b4f89f6cc91eca1b67
Dogelix <yeatmanjake@gmail.com>
2020-03-12 15:29:23 +0000

    Final Checks and basic set up of new NotLD Level

--- Assets/Scripts/AI/GroundMovement.cs
+++ Assets/Scripts/AI/GroundMovement.cs
@@ -13,6 +13,8 @@ public class GroundMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponentInChildren<Animator>().SetTrigger("ZombieWalk");
+
         SetTarget();
         soundManager = FindObjectOfType<SoundManager>();
         // Start Quacking
