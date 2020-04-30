2bcf0652410ea9734057b5c541f07daa7efc251f
Rad5002 <ryan5002@hotmail.co.uk>
2020-04-30 13:51:11 +0100

    MainMenu
    + Added Main Menu 1
    + Added Options Menu (Not working)
    + Added canvas to DuckOpsProgrammer Level

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSystem : MonoBehaviour
+{
+
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("Scenes/DuckOpsProgrammerLevel");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MainMenuSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void SceneName()
+    {
+
+    }
+
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("Scenes/DuckOpsProgrammerLevel");
+    }
+}
