bd87b65ebb15212c41940783b4a34f5e7823a5ed
Yaro <47662772+YaroWroot@users.noreply.github.com>
2020-05-07 13:56:30 +0100

    Rocket
    Implemented rocket, still needs to have textures applied and put into weapon change system

---
+++ Assets/Scripts/Weapons/Launcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using static SoundManager;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+    public SteamVR_Action_Boolean _fireAction = null;
+    public float propulsionForce;
+
+    private SteamVR_Behaviour_Pose _pose;
+    private float nextFire;
+    public float firerate = 0.25f;
+    private Transform rocketTransform;
+    void Awake()
+    {
+        _pose = GetComponentInParent<SteamVR_Behaviour_Pose>();
+    }
+
+    void Start()
+    {
+        SetInitialReferences();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_fireAction.GetStateDown(_pose.inputSource) && Time.time > nextFire)
+        {
+            nextFire = Time.time + firerate;
+
+            SpawnRocket();
+        }
+    }
+
+    void SpawnRocket()
+    {
+        GameObject rocket = (GameObject) Instantiate(rocketPrefab, rocketTransform.transform.TransformPoint(0, 0, 2f), rocketTransform.rotation);
+        rocket.GetComponent<Rigidbody>().AddForce(rocketTransform.forward * propulsionForce, ForceMode.Impulse);
+        Destroy(rocket, 3);
+    }
+
+    void SetInitialReferences()
+    {
+        rocketTransform = transform;
+    }
+}
---
+++ Assets/Scripts/Weapons/RocketExplosion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using static SoundManager;
+
+public class RocketExplosion : MonoBehaviour
+{
+    public float blastRadius;
+
+    private Collider[] hitColliders;
+
+    void OnCollisionEnter(Collision col)
+    {
+        DoExplosion(col.contacts[0].point);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void DoExplosion(Vector3 explosionPoint)
+    {
+        hitColliders = Physics.OverlapSphere(explosionPoint, blastRadius);
+
+        foreach(Collider hitcol in hitColliders)
+        {
+            if (hitcol.gameObject.tag == "Enemy")
+            {
+                if (hitcol.gameObject.layer == LayerMask.NameToLayer("GroundEnemy"))
+                {
+                    hitcol.transform.gameObject.GetComponent<GroundAgentCollision>().RaycastDestroy();
+
+                }
+                else
+                {
+                    hitcol.transform.gameObject.GetComponent<CollisionDetection>().RaycastDestroy();
+                }
+            }
+       
+        }
+    }
+}
