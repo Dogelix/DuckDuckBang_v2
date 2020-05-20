3853460e7858808259dd73fcdd18ddbcf8805c4f
Jake Yeatman <jake.yeatman@rds.global>
2020-05-20 17:08:41 +0100

    GameMode_SO Back in Business

--- Assets/Scripts/Modular Game Modes/WaveGameMode_SO.cs
+++ Assets/Scripts/Modular Game Modes/WaveGameMode_SO.cs
@@ -32,7 +32,7 @@ public class WaveGameMode_SO : GameMode_SO
     /// e.g. Upper 2:1 Lower
     /// </summary>
     [Tooltip("A ratio where Upper == Ground and Lower == Air e.g. Upper 2:1 Lower")]
-    public Limits _enemyWeighting;
+    public LimitedLimits _enemyWeighting;
 
     /// <summary>
     /// A fraction where Lower is the chance in Upper
@@ -44,18 +44,20 @@ public class WaveGameMode_SO : GameMode_SO
     public List<GameObject> _agents;
 
     // Private holder for all transforms within spawn containers
-    private List<Transform> _flySpawnLocations;
-    private List<Transform> _groundSpawnLocations;
+    private List<GameObject> _flySpawnLocations;
+    private List<GameObject> _groundSpawnLocations;
 
     public bool _countUp;
+    public bool _spawnBaskets;
 
-
-    private int ratioCountUpper;
-    private int ratioCountLower;
+    private bool spawnLock = false;
 
     public void RemoveAgent(GameObject obj)
     {
         _agents.Remove(obj);
+
+        if ( _agents.Count == 0 )
+            spawnLock = false;
     }
 
     /// <summary>
@@ -64,14 +66,20 @@ public class WaveGameMode_SO : GameMode_SO
     /// <param name="value">The Spawn Locations as a List<Transform></param>
     public override void Init(object value)
     {
-        base._type = GameModeTypes.Wave;
-
-        _flySpawnLocations = GameObject.FindGameObjectsWithTag("FlySpawn").Select(x => x.transform).ToList();
-        _groundSpawnLocations = GameObject.FindGameObjectsWithTag("GroundSpawn").Select(x => x.transform).ToList();
-
-        if (!_countUp)
+        DebugPlus.LogOnScreen("WaveGameMode_SO.Init()");
+        try
         {
-            _currentWave = _waves;
+            base._type = GameModeTypes.Wave;
+
+            _flySpawnLocations = GameObject.FindGameObjectsWithTag("FlySpawn").ToList();
+            _groundSpawnLocations = GameObject.FindGameObjectsWithTag("GroundSpawn").ToList();
+
+            _currentWave = 0;
+            _currentWaveSize = _waveBaseValue;
+        }
+        catch ( System.Exception e )
+        {
+            Debug.LogError(e.Message);
         }
 
         base.Init(value);
@@ -81,78 +89,54 @@ public class WaveGameMode_SO : GameMode_SO
     {
         var chance  = Random.Range(0, _randomBasketSpawn.Upper + 1);
 
-        if(chance <= _randomBasketSpawn.Lower )
+        if ( chance <= _randomBasketSpawn.Lower )
         {
 
         }
 
-        if(_agents.Count == 0)
+        if (_agents.Count == 0 && spawnLock == false)
         {
-            //Debug.Log("Agents: " + _agents.Count);
-            if (_countUp)
-            {
-                if(_waves != 1)
-                {
-                    _currentWaveSize += Random.Range(_waveIncreaseValue.Lower, _waveIncreaseValue.Upper + 1);
-                    _waves++;
-                }
-                else
-                {
-                    _currentWaveSize = _waveBaseValue;
-                }
-            }
-            else
-            {
-                if(_waves == _currentWave)
-                {
-                    _currentWaveSize = _waveBaseValue;
-                }
-                else
-                {
-                    _currentWaveSize += Random.Range(_waveIncreaseValue.Lower, _waveIncreaseValue.Upper + 1);
-                    _currentWave--;
-                }
+            spawnLock  = true;
+            _currentWave++;
+            
+            //if wave is 2+ do this
+            if(_currentWave != 1)
+                _currentWaveSize = _currentWaveSize + Random.Range(_waveIncreaseValue.Lower, _waveIncreaseValue.Upper + 1);
 
-                if(_currentWave == 0)
-                {
-                    _gameOver = true;
-                }
-            }
-
-            ratioCountUpper = _enemyWeighting.Upper;
-            ratioCountLower = _enemyWeighting.Lower;
+            int flying = (int)(_currentWaveSize * _enemyWeighting.Lower);
 
             float spawnDelay = 0f;
             for (int count = 0; count < _currentWaveSize; count++)
             {
-                StartCoroutine(Spawn(spawnDelay));
+                if(flying != 0 )
+                {
+                    StartCoroutine(Spawn(spawnDelay, true));
+                    flying--;
+                }
+                else
+                {
+                    StartCoroutine(Spawn(spawnDelay, false));
+                }
+
                 spawnDelay += 1.5f;
             }           
         }
     }
 
-    private IEnumerator Spawn(float spawnDelay)
+    private IEnumerator Spawn(float spawnDelay, bool flying)
     {
         yield return new WaitForSeconds(spawnDelay);
 
-        if (ratioCountUpper != 0)
+        if (!flying)
         {
-            _agents.Add(Instantiate(GameAssets.i.ZombieWalkingDuck.gameObject, _groundSpawnLocations[Random.Range(0, _groundSpawnLocations.Count)].position, Quaternion.identity)); //Comment out for unlimted spawn
-            ratioCountUpper--;
+            _agents.Add(Instantiate(GameAssets.i.ZombieWalkingDuck.gameObject, _groundSpawnLocations[Random.Range(0, _groundSpawnLocations.Count)].transform.position, Quaternion.identity)); //Comment out for unlimted spawn
         }
         else
         {
-            var t = Instantiate(GameAssets.i.RegularFlyingDuck.gameObject, _flySpawnLocations[Random.Range(0, _flySpawnLocations.Count)].position, Quaternion.identity);
+            var t = Instantiate(GameAssets.i.RegularFlyingDuck.gameObject, _flySpawnLocations[Random.Range(0, _flySpawnLocations.Count)].transform.position, Quaternion.identity);
             t.GetComponent<FlockAgent>().SetCollider(); //Comment out for unlimted spawn
             Flock.agents.Add(t.GetComponent<FlockAgent>()); //Comment out for unlimted spawn
             _agents.Add(t); //Comment out for unlimted spawn
-            ratioCountLower--;
-        }
-
-        if (ratioCountLower == 0)
-        {
-            ratioCountUpper = _enemyWeighting.Upper;
-            ratioCountLower = _enemyWeighting.Lower;
         }
     }
 
@@ -160,7 +144,7 @@ public class WaveGameMode_SO : GameMode_SO
     {
         foreach (var s in _flySpawnLocations)
         {
-            Debug.Log("Spawn Loc: " + s.name + " Position: " + s.position + " [Flying Duck]");
+            Debug.Log("Spawn Loc: " + s.name + " Position: " + s.transform.position + " [Flying Duck]");
         } 
     }
 
@@ -171,6 +155,11 @@ public class WaveGameMode_SO : GameMode_SO
         foreach (var agent in allAgents)
         {
             Destroy(agent);
+        }
+
+        foreach ( var agent  in Flock.agents )
+        {
+            Destroy(agent.gameObject);
         }
 
         _agents.Clear();
